a3040e36f4a4c8429dad15929f0b32e636d1aff7
borisrozumnuk <borisrozumnuk@gmail.com>
2021-06-07 13:53:20 +0200

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_require = {
 
 setup(
     name='cognitojwt',
-    version='1.3.0',
+    version='1.4.0',
     description='Decode and verify Amazon Cognito JWT tokens',
     long_description=long_description,
     long_description_content_type="text/markdown",
